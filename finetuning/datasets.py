65fae8523dbcba0b2fd8329130e85d8be7be49c2
Aniketh Janardhan Reddy <anikethjr@gmail.com>
2024-02-26 15:15:57 -0800

    Bug fixes

--- finetuning/datasets.py
+++ finetuning/datasets.py
@@ -310,11 +310,10 @@ class PairwiseMPRADataset(torch.utils.data.Dataset):
             if coin_flip:
                 ref_seq = np.flip(ref_seq, axis=0)
                 alt_seq = np.flip(alt_seq, axis=0)
-                variant_effect = np.flip(variant_effect, axis=0)
 
                 # order of bases is ACGT, so reverse-complement is just flipping the sequence
-                ref_seq = np.flip(ref_seq, axis=2)
-                alt_seq = np.flip(alt_seq, axis=2)
+                ref_seq = np.flip(ref_seq, axis=1)
+                alt_seq = np.flip(alt_seq, axis=1)
 
         ref_seq = torch.tensor(ref_seq.copy()).float()
         alt_seq = torch.tensor(alt_seq.copy()).float()
