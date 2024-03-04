29bc942b0b52bc1d93fa4a54db4a1a405d38aef8
Aniketh Janardhan Reddy <anikethjr@gmail.com>
2024-03-04 15:04:38 -0800

    Try deepspeed again on 8x TITAN

--- finetuning/train_pairwise_with_enformer_data_parallel_deepspeed.py
+++ finetuning/train_pairwise_with_enformer_data_parallel_deepspeed.py
@@ -9,7 +9,7 @@ from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
 from lightning.pytorch.loggers import WandbLogger
 from lightning.pytorch.strategies import DDPStrategy, DeepSpeedStrategy
 from lightning.pytorch.utilities.combined_loader import CombinedLoader
-from models import PairwiseWithOriginalDataJointTraining
+from models import PairwiseWithOriginalDataJointTrainingFloatPrecision
 
 torch.manual_seed(97)
 torch.set_float32_matmul_precision("medium")
@@ -22,14 +22,12 @@ def parse_args():
     parser.add_argument("enformer_data_path", type=str)
     parser.add_argument("run_name", type=str)
     parser.add_argument("save_dir", type=str)
-    parser.add_argument("--lr", type=float, default=1e-5)
-    parser.add_argument("--initial_denom_lr", type=float, default=1.0)
+    parser.add_argument("--lr", type=float, default=0.0005)
     parser.add_argument("--train_bn", action=BooleanOptionalAction, default=True)
     parser.add_argument("--batch_size", type=int, default=4)
     parser.add_argument("--train_n_pairs", type=int, default=100_000)
     parser.add_argument("--val_n_pairs", type=int, default=5_000)
     parser.add_argument("--max_epochs", type=int, default=20)
-    parser.add_argument("--max_steps", type=int, default=250000)
     parser.add_argument("--enformer_checkpoint", type=str, default=None)
     parser.add_argument("--state_dict_subset_prefix", type=str, default=None)
     return parser.parse_args()
@@ -76,7 +74,7 @@ def main():
     train_dl = CombinedLoader(
         [
             torch.utils.data.DataLoader(
-                pairwise_train_ds, batch_size=args.batch_size, shuffle=True
+                pairwise_train_ds, batch_size=args.batch_size * 4, shuffle=True
             ),
             torch.utils.data.DataLoader(
                 human_enformer_train_ds, batch_size=args.batch_size
@@ -85,12 +83,12 @@ def main():
                 mouse_enformer_train_ds, batch_size=args.batch_size
             ),
         ],
-        mode="min_size",
+        mode="max_size_cycle",
     )
 
     val_dl = [
         torch.utils.data.DataLoader(
-            pairwise_val_ds, batch_size=args.batch_size, shuffle=False
+            pairwise_val_ds, batch_size=args.batch_size * 4, shuffle=False
         ),
         torch.utils.data.DataLoader(human_enformer_val_ds, batch_size=args.batch_size),
         torch.utils.data.DataLoader(mouse_enformer_val_ds, batch_size=args.batch_size),
@@ -111,6 +109,9 @@ def main():
     )
 
     os.environ["SLURM_JOB_NAME"] = "interactive"
+    # get number of gpus
+    n_gpus = torch.cuda.device_count()
+    print(f"Number of GPUs: {n_gpus}")
     trainer = Trainer(
         accelerator="gpu",
         devices="auto",
@@ -122,17 +123,22 @@ def main():
         default_root_dir=args.save_dir,
         callbacks=[checkpointing_cb, early_stopping_cb],
         precision="16-mixed",
-        accumulate_grad_batches=(64 // (args.batch_size * 8)),
+        accumulate_grad_batches=(
+            64 // (args.batch_size * n_gpus)
+        ),  # original Enformer model was trained with 64 batch size using the same 0.0005 learning rate
         strategy=DeepSpeedStrategy(
+            zero_optimization=True,
+            stage=3,
             logging_batch_size_per_gpu=args.batch_size,
-            stage=2,
             offload_optimizer=False,
             offload_parameters=True,
-            min_loss_scale=0,
+            min_loss_scale=1,
+            pin_memory=True,
+            zero_allow_untested_optimizer=False,
         ),
     )
 
-    model = PairwiseWithOriginalDataJointTraining(
+    model = PairwiseWithOriginalDataJointTrainingFloatPrecision(
         lr=args.lr,
         n_total_bins=pairwise_train_ds.get_total_n_bins(),
         checkpoint=args.enformer_checkpoint,
