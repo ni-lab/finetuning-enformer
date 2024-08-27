f9430d1eab3d4767e54a341d772836fabc8014c1
Aniketh Janardhan Reddy <anikethjr@gmail.com>
2024-08-26 22:10:21 -0700

    Bug fix

--- finetuning/train_pairwise_regression_with_MPRA_data_parallel_h5_dynamic_sampling_dataset.py
+++ finetuning/train_pairwise_regression_with_MPRA_data_parallel_h5_dynamic_sampling_dataset.py
@@ -177,7 +177,7 @@ def main():
         accumulate_grad_batches=(
             64 // (args.batch_size * n_gpus)
         ),  # original Enformer model was trained with 64 batch size using the same 0.0005 learning rate
-        strategy="ddp",
+        strategy="ddp_find_unused_parameters_true",
     )
 
     model = PairwiseRegressionWithMalinoisMPRAJointTrainingFloatPrecision(
